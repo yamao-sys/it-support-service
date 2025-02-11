4dbf89eaef30f6518edec1eddb18dd80ff5b19ae
yamao-sys <yamaopanku@gmail.com>
2025-02-11 16:37:09 +0900

    companiesテーブルの追加

---
+++ migrations/ddl/20250211163350-CreateCompanies.sql
@@ -0,0 +1,14 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS companies(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	final_tax_return VARCHAR(255) NOT NULL,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS companies;
