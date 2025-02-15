0dc2057ba8a9e85a52b4dc010016bb161cfb6435
yamao-sys <yamaopanku@gmail.com>
2025-02-15 13:55:05 +0900

    CIで静的解析が効くかテスト

--- frontend/it-support/packages/ui/src/button.tsx
+++ frontend/it-support/packages/ui/src/button.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 interface ButtonProps {
   children: ReactNode;
@@ -12,7 +12,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={() => alert(`Hello from your ${appName} apps!`)}
     >
       {children}
     </button>
