45566df6f081969573d86d472cbbe8343e03f797
yamao-sys <yamaopanku@gmail.com>
2025-03-05 23:45:28 +0900

    フロントエンドのCI(静的解析)のlint重複をtscに変更

--- frontend/it-support/apps/docs/app/page.tsx
+++ frontend/it-support/apps/docs/app/page.tsx
@@ -63,9 +63,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName='docs' className={styles.secondary}>
-          Open alert
-        </Button>
+        <Button>Open alert</Button>
       </main>
       <footer className={styles.footer}>
         <a
