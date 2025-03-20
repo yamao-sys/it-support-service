8a27d500be31f5b1738197ef47d5e7dfb91263f4
yamao-sys <yamaopanku@gmail.com>
2025-03-20 17:05:57 +0900

    RegistrationのフロントエンドのCloud Runへのデプロイ

--- frontend/it-support/apps/registration/next.config.js
+++ frontend/it-support/apps/registration/next.config.js
@@ -1,3 +1,9 @@
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -5,6 +11,8 @@ const nextConfig = {
       bodySizeLimit: "10mb",
     },
   },
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
