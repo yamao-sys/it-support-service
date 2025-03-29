e6ab5991425e317f17bf3bc8412ed18d5fad45cd
yamao-sys <yamaopanku@gmail.com>
2025-03-29 14:37:07 +0900

    Business Frontendのcompanies.apiクライアントの作成

---
+++ frontend/it-support/apps/business/apis/companies.api.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { paths } from "@/apis/generated/companies/apiSchema";
+import createClient from "openapi-fetch";
+import { CompanySignInInput } from "./types";
+import { getRequestHeaders } from "./csrf.api";
+import { cookies } from "next/headers";
+
+const client = createClient<paths>({
+  baseUrl: `${process.env.BUSINESS_API_ENDPOINT_URI}/`,
+  credentials: "include",
+});
+
+export async function postCompanySignIn(input: CompanySignInInput) {
+  const { response } = await client.POST("/companies/signIn", {
+    ...(await getRequestHeaders()),
+    body: input,
+  });
+  if (response.status === 500) {
+    throw Error("Internal Server Error");
+  }
+  if (response.status === 400) {
+    return "メールアドレスまたはパスワードが正しくありません";
+  }
+
+  // NOTE: クライアントにCookieをセット
+  const setCookie = response.headers.get("set-cookie");
+  if (!setCookie) {
+    throw Error();
+  }
+  const token = setCookie?.split(";")[0]?.split("=")[1];
+  if (!token) {
+    throw Error();
+  }
+
+  // TODO: cookieの属性は環境変数に切り出す
+  (await cookies()).set({
+    name: "token",
+    value: token,
+    secure: true,
+    sameSite: "none",
+    httpOnly: true,
+  });
+
+  return "";
+}
