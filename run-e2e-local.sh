c702cc10b5bc7f60a9def48d6131e146a9dda988
yamao-sys <yamaopanku@gmail.com>
2025-03-29 16:11:08 +0900

    ローカルでBusinessでもE2Eを実行できるよう

--- run-e2e-local.sh
+++ run-e2e-local.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# シーディング
+docker-compose run --rm business_api sh -c 'make test-seed-local'
+
 # テスト用の環境でAPIサーバを起動
 docker-compose exec -d registration_api godotenv -f .env.test.local go run main.go
+docker-compose exec -d business_api godotenv -f .env.test.local go run main.go
 
 # テスト実行
 docker-compose exec frontend pnpm test:e2e
 
 # テスト用DBのリセット
-docker-compose run --rm db mysql -h db -u root -p -e "DROP DATABASE it_support_test; CREATE DATABASE it_support_test;"
-docker-compose run --rm migrations sh -c 'make migrate-test-local'
+docker-compose run --rm db mysql -h db -u root -p -e "DROP DATABASE it_support_test; CREATE DATABASE it_support_test; DROP DATABASE it_support_business_test; CREATE DATABASE it_support_business_test;"
+docker-compose run --rm migrations sh -c 'make migrate-test-local && make migrate-business-test-local'
