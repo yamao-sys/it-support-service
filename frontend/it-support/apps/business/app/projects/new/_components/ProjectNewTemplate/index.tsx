3b8f67db49cc2dfc4b1ecb2f0fc90a03e58ee52f
yamao-sys <yamaopanku@gmail.com>
2025-04-07 00:02:29 +0900

    Business FrontendのProject作成のTemplateコンポーネント作成

---
+++ frontend/it-support/apps/business/app/projects/new/_components/ProjectNewTemplate/index.tsx
@@ -0,0 +1,12 @@
+import ProjectStoreForm from "@/app/projects/_components/ProjectStoreForm";
+import { FC } from "react";
+
+const ProjectNewTemplate: FC = () => {
+  return (
+    <>
+      <ProjectStoreForm />
+    </>
+  );
+};
+
+export default ProjectNewTemplate;
