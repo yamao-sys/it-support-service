3c1d7e88ae56767ab4ffd1f68cedac90897179e7
yamao-sys <yamaopanku@gmail.com>
2025-04-08 00:42:53 +0900

    Business Frontend: ProjectのAPIのbodySerializerの修正

--- frontend/it-support/apps/business/apis/projects.api.ts
+++ frontend/it-support/apps/business/apis/projects.api.ts
@@ -14,21 +14,21 @@ export async function postProjectCreate(input: ProjectStoreInput) {
   const { data, response } = await client.POST("/projects", {
     ...(await getRequestHeaders()),
     body: input,
-    bodySerializer(body) {
+    bodySerializer() {
       const reqBody: { [key: string]: string | number | boolean } = {};
-      if (body) {
-        for (const [key, value] of Object.entries(input)) {
-          if (value instanceof Date) {
-            reqBody[key] = value
-              .toLocaleDateString("ja-JP", { year: "numeric", month: "2-digit", day: "2-digit" })
-              .replaceAll("/", "-");
-          } else if (["minBudget", "maxBudget"].includes(key)) {
+      for (const [key, value] of Object.entries(input)) {
+        if (value instanceof Date) {
+          reqBody[key] = value
+            .toLocaleDateString("ja-JP", { year: "numeric", month: "2-digit", day: "2-digit" })
+            .replaceAll("/", "-");
+        } else if (["minBudget", "maxBudget"].includes(key)) {
+          if (value) {
             reqBody[key] = Number(value);
-          } else if (["isActive"].includes(key)) {
-            reqBody[key] = Boolean(value);
-          } else {
-            reqBody[key] = value;
           }
+        } else if (["isActive"].includes(key)) {
+          reqBody[key] = value === "1" ? true : false;
+        } else {
+          reqBody[key] = value;
         }
       }
       return JSON.stringify(reqBody);
