d3c992b18f5d1944af3f98f247c30cfa98e604d1
yamao-sys <yamaopanku@gmail.com>
2025-04-09 01:15:22 +0900

    Business Frontend: 型追加

--- frontend/it-support/apps/business/types/index.d.ts
+++ frontend/it-support/apps/business/types/index.d.ts
@@ -12,3 +12,8 @@ export type ProjectStoreInput =
   components["requestBodies"]["ProjectStoreInput"]["content"]["application/json"];
 
 export type ProjectValidationError = components["schemas"]["ProjectValidationError"];
+
+export type ProjectResponse =
+  components["responses"]["ProjectResponse"]["content"]["application/json"];
+
+export type Project = components["schemas"]["Project"];
