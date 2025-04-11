a3d8b0f9eea1c039e088dcad69f0707c03a93960
yamao-sys <yamaopanku@gmail.com>
2025-04-12 00:00:09 +0900

    Projectの追加・更新のフォームコンポーネントの作成

--- frontend/it-support/apps/business/app/projects/_components/ProjectStoreForm/index.tsx
+++ frontend/it-support/apps/business/app/projects/_components/ProjectStoreForm/index.tsx
@@ -1,64 +1,54 @@
 "use client";
 
-import { postProjectCreate } from "@/apis/projects.api";
 import { ProjectStoreInput, ProjectValidationError } from "@/types";
-import { FC, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
+import { FC } from "react";
+import { Control, Controller, UseFormRegister } from "react-hook-form";
 import BaseFormInput from "@repo/ui/BaseFormInput";
 import BaseButton from "@repo/ui/BaseButton";
 import BaseFormTextarea from "@repo/ui/BaseFormTextarea";
 import ReactDatePicker from "react-datepicker";
 
-const INITIAL_VALIDATION_ERRORS = {
-  title: [],
-  description: [],
-  startDate: [],
-  endDate: [],
-  minBudget: [],
-  maxBudget: [],
-  isActive: [],
+type Props = {
+  register: UseFormRegister<ProjectStoreInput>;
+  control: Control<ProjectStoreInput>;
+  onSubmit: (e?: React.BaseSyntheticEvent) => Promise<void>;
+  validationErrors: ProjectValidationError;
 };
 
-const ProjectStoreForm: FC = () => {
-  // TODO: register, post関数をprops化
-  const { register, control, handleSubmit } = useForm<ProjectStoreInput>();
-
-  const [validationErrors, setValidationErrors] =
-    useState<ProjectValidationError>(INITIAL_VALIDATION_ERRORS);
-
-  const router = useRouter();
-
-  const onSubmit = handleSubmit(async (data) => {
-    const errors = await postProjectCreate(data);
-    if (Object.keys(errors).length > 0) {
-      setValidationErrors(errors);
-      return;
-    }
-
-    window.alert("案件を更新しました!");
-    router.push("/");
-  });
-
+const ProjectStoreForm: FC<Props> = ({ register, control, onSubmit, validationErrors }: Props) => {
   return (
     <>
       <form onSubmit={onSubmit}>
         <div className='mt-8'>
-          <BaseFormInput
-            id='title'
-            label='案件タイトル'
-            type='text'
-            {...register("title", { required: "案件タイトルは必須です" })}
-            validationErrorMessages={validationErrors.title ?? []}
+          <Controller
+            control={control}
+            name='title'
+            render={({ field }) => (
+              <BaseFormInput
+                id='title'
+                label='案件タイトル'
+                type='text'
+                {...field}
+                value={field.value ?? ""}
+                validationErrorMessages={validationErrors.title ?? []}
+              />
+            )}
           />
         </div>
 
         <div className='mt-8'>
-          <BaseFormTextarea
-            id='description'
-            label='案件概要'
-            {...register("description", { required: "案件概要は必須です" })}
-            validationErrorMessages={validationErrors.description ?? []}
+          <Controller
+            control={control}
+            name='description'
+            render={({ field }) => (
+              <BaseFormTextarea
+                id='description'
+                label='案件概要'
+                {...field}
+                value={field.value ?? ""}
+                validationErrorMessages={validationErrors.description ?? []}
+              />
+            )}
           />
         </div>
 
@@ -125,52 +115,61 @@ const ProjectStoreForm: FC = () => {
 
         <div className='mt-8 flex items-between'>
           <div className='mr-4 w-1/3'>
-            <BaseFormInput
-              id='minBudget'
-              label='予算(下限)'
-              type='number'
-              {...register("minBudget")}
-              validationErrorMessages={validationErrors.minBudget ?? []}
+            <Controller
+              control={control}
+              name='minBudget'
+              render={({ field }) => (
+                <BaseFormInput
+                  id='minBudget'
+                  label='予算(下限)'
+                  type='number'
+                  {...field}
+                  value={field.value ?? ""}
+                  validationErrorMessages={validationErrors.minBudget ?? []}
+                />
+              )}
             />
           </div>
           <div className='w-1/3'>
-            <BaseFormInput
-              id='maxBudget'
-              label='予算(上限)'
-              type='number'
-              {...register("maxBudget")}
-              validationErrorMessages={validationErrors.maxBudget ?? []}
+            <Controller
+              control={control}
+              name='maxBudget'
+              render={({ field }) => (
+                <BaseFormInput
+                  id='maxBudget'
+                  label='予算(上限)'
+                  type='number'
+                  {...field}
+                  value={field.value ?? ""}
+                  validationErrorMessages={validationErrors.maxBudget ?? []}
+                />
+              )}
             />
           </div>
         </div>
 
         <div className='mt-8'>
-          <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left'>
-            <span className='font-bold'>公開フラグ</span>
+          <label className='inline-flex items-center cursor-pointer'>
+            <input
+              type='checkbox'
+              className='sr-only peer'
+              required={false}
+              {...register("isActive")}
+            />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+            <span className='ms-3 text-sm font-medium text-gray-900 dark:text-gray-300'>
+              公開フラグ
+            </span>
           </label>
-
-          <div className='flex space-x-2'>
-            {[
-              { id: "active", label: "公開", value: 1 },
-              { id: "notActive", label: "非公開", value: 0 },
-            ].map((option) => (
-              <div key={option.label} className='cursor-pointer'>
-                <input
-                  id={option.id}
-                  type='radio'
-                  value={option.value}
-                  {...register("isActive", { required: "公開フラグは必須です。" })}
-                  className='peer hidden'
-                />
-                <label
-                  htmlFor={option.id}
-                  className='px-4 py-2 rounded-full bg-gray-200 peer-checked:bg-blue-500 peer-checked:text-white'
-                >
-                  {option.label}
-                </label>
-              </div>
-            ))}
-          </div>
+          {!!validationErrors.isActive?.length && (
+            <div className='w-full pt-5 text-left'>
+              {validationErrors.isActive.map((message, i) => (
+                <p key={i} className='text-red-400'>
+                  {message}
+                </p>
+              ))}
+            </div>
+          )}
         </div>
 
         <div className='w-full flex justify-center'>
