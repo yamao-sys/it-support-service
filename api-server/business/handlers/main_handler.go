c304bf26c5818c469b024a352d2cfff6c7ade5e9
yamao-sys <yamaopanku@gmail.com>
2025-05-18 11:30:46 +0900

    BusinessのoperationId調整をmain_handlerに反映

--- api-server/business/handlers/main_handler.go
+++ api-server/business/handlers/main_handler.go
@@ -10,19 +10,19 @@ type MainHandler interface {
 	GetCsrf(ctx context.Context, request businessapi.GetCsrfRequestObject) (businessapi.GetCsrfResponseObject, error)
 
 	// handlers /supporters
-	PostSupportersSignIn(ctx context.Context, request businessapi.PostSupportersSignInRequestObject) (businessapi.PostSupportersSignInResponseObject, error)
+	PostSupporterSignIn(ctx context.Context, request businessapi.PostSupporterSignInRequestObject) (businessapi.PostSupporterSignInResponseObject, error)
 
 	// handlers /companies
-	PostCompaniesSignIn(ctx context.Context, request businessapi.PostCompaniesSignInRequestObject) (businessapi.PostCompaniesSignInResponseObject, error)
+	PostCompanySignIn(ctx context.Context, request businessapi.PostCompanySignInRequestObject) (businessapi.PostCompanySignInResponseObject, error)
 
 	// handlers /projects
 	GetProjects(ctx context.Context, request businessapi.GetProjectsRequestObject) (businessapi.GetProjectsResponseObject, error)
-	PostProjects(ctx context.Context, request businessapi.PostProjectsRequestObject) (businessapi.PostProjectsResponseObject, error)
+	PostProject(ctx context.Context, request businessapi.PostProjectRequestObject) (businessapi.PostProjectResponseObject, error)
 	GetProject(ctx context.Context, request businessapi.GetProjectRequestObject) (businessapi.GetProjectResponseObject, error)
 	PutProject(ctx context.Context, request businessapi.PutProjectRequestObject) (businessapi.PutProjectResponseObject, error)
 
 	// handlers /plans
-	PostPlans(ctx context.Context, request businessapi.PostPlansRequestObject) (businessapi.PostPlansResponseObject, error)
+	PostPlan(ctx context.Context, request businessapi.PostPlanRequestObject) (businessapi.PostPlanResponseObject, error)
 }
 
 type mainHandler struct {
@@ -48,13 +48,13 @@ func (mh *mainHandler) GetCsrf(ctx context.Context, request businessapi.GetCsrfR
 	return res, err
 }
 
-func (mh *mainHandler) PostSupportersSignIn(ctx context.Context, request businessapi.PostSupportersSignInRequestObject) (businessapi.PostSupportersSignInResponseObject, error) {
-	res, err := mh.supportersHandler.PostSupportersSignIn(ctx, request)
+func (mh *mainHandler) PostSupporterSignIn(ctx context.Context, request businessapi.PostSupporterSignInRequestObject) (businessapi.PostSupporterSignInResponseObject, error) {
+	res, err := mh.supportersHandler.PostSupporterSignIn(ctx, request)
 	return res, err
 }
 
-func (mh *mainHandler) PostCompaniesSignIn(ctx context.Context, request businessapi.PostCompaniesSignInRequestObject) (businessapi.PostCompaniesSignInResponseObject, error) {
-	res, err := mh.companiesHandler.PostCompaniesSignIn(ctx, request)
+func (mh *mainHandler) PostCompanySignIn(ctx context.Context, request businessapi.PostCompanySignInRequestObject) (businessapi.PostCompanySignInResponseObject, error) {
+	res, err := mh.companiesHandler.PostCompanySignIn(ctx, request)
 	return res, err
 }
 
@@ -63,8 +63,8 @@ func (mh *mainHandler) GetProjects(ctx context.Context, request businessapi.GetP
 	return res, err
 }
 
-func (mh *mainHandler) PostProjects(ctx context.Context, request businessapi.PostProjectsRequestObject) (businessapi.PostProjectsResponseObject, error) {
-	res, err := mh.projectsHandler.PostProjects(ctx, request)
+func (mh *mainHandler) PostProject(ctx context.Context, request businessapi.PostProjectRequestObject) (businessapi.PostProjectResponseObject, error) {
+	res, err := mh.projectsHandler.PostProject(ctx, request)
 	return res, err
 }
 
@@ -78,7 +78,7 @@ func (mh *mainHandler) PutProject(ctx context.Context, request businessapi.PutPr
 	return res, err
 }
 
-func (mh *mainHandler) PostPlans(ctx context.Context, request businessapi.PostPlansRequestObject) (businessapi.PostPlansResponseObject, error) {
-	res, err := mh.plansHandler.PostPlans(ctx, request)
+func (mh *mainHandler) PostPlan(ctx context.Context, request businessapi.PostPlanRequestObject) (businessapi.PostPlanResponseObject, error) {
+	res, err := mh.plansHandler.PostPlan(ctx, request)
 	return res, err
 }
