045681a14e39314d082b00d826d80b3592b0d425
yamao-sys <yamaopanku@gmail.com>
2025-06-03 09:30:58 +0900

    Business FrontendでOpenAPI Generatorによるコード自動生成

---
+++ frontend/it-support/apps/business/apis/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+ 
+export * from "./CompaniesApi";
+export * from "./CsrfApi";
+export * from "./PlansApi";
+export * from "./ProjectsApi";
+export * from "./SupportersApi";
+export * from "./ToProjectsApi";
