4637315cf10ceb069b76ca0abd141a7a9298d1df
yamao-sys <yamaopanku@gmail.com>
2025-06-03 11:29:42 +0900

    未使用となったRegistration Frontendのopenapi-typescript製のapiSchema削除

--- frontend/it-support/apps/registration/types/index.d.ts
+++ frontend/it-support/apps/registration/types/index.d.ts
@@ -1,14 +1,3 @@
-import { components } from "@/apis/generated/apiSchema";
-
 export type FormType = "supporter" | "company";
 
 export type PhaseType = "input" | "confirm" | "thanks";
-
-export type SupporterSignUpInput = components["schemas"]["SupporterSignUpInput"];
-
-export type SupporterSignUpValidationError =
-  components["schemas"]["SupporterSignUpResponse"]["errors"];
-
-export type CompanySignUpInput = components["schemas"]["CompanySignUpInput"];
-
-export type CompanySignUpValidationError = components["schemas"]["CompanySignUpResponse"]["errors"];
