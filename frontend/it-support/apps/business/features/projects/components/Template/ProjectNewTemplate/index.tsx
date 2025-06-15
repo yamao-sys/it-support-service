cbc391d6f0a9d8919d6d300e916782810dcb91cb
yamao-sys <yamaopanku@gmail.com>
2025-06-15 17:02:30 +0900

    Business Frontendのprojectsをfeaturesへ移行: 案件作成のcomponent

---
+++ frontend/it-support/apps/business/features/projects/components/Template/ProjectNewTemplate/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import ProjectNewContainer from "../../ProjectNewContainer";
+
+const ProjectNewTemplate: FC = () => {
+  return (
+    <>
+      <ProjectNewContainer />
+    </>
+  );
+};
+
+export default ProjectNewTemplate;
